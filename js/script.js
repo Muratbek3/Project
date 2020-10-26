ec3c6eed2ebdfed4ceb690593930ad29d26af5b0
Murat <bek.m4n@gmail.com>
2020-10-26 10:53:55 +0300

    first commit

---
+++ js/script.js
@@ -0,0 +1,14 @@
+/* const result= confirm("Are you here?");
+/* console.log(result); */
+
+/* const answer = +prompt("Вам есть 18?", "18");
+console.log(typeof(answer)); */ 
+
+
+const answers = [];
+
+answers[0] = prompt("Как вас зовут?", "");
+answers[1] = prompt("Какая у вас фамилия?", "");
+answers[2] = prompt("Сколько вам лет?", "");
+
+document.write(answers);
