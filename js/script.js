2ac1c783efad15fac415916dfe41e739c413e0b2
Murat <bek.m4n@gmail.com>
2020-10-27 07:44:58 +0300

    js/script.js

--- js/script.js
+++ js/script.js
@@ -3,16 +3,32 @@
 
 /* const answer = +prompt("Вам есть 18?", "18");
 console.log(typeof(answer)); */ 
+let meltText = "от 1 до 10";
 
-
- const answers = [];
 
 /* answers[0] = prompt("Как вас зовут?", "");
 answers[1] = prompt("Какая у вас фамилия?", "");
 answers[2] = prompt("Сколько вам лет?", "");
 
 document.write(answers); */
+let numberOfFilms = prompt("Сколько фильмов вы уже посмотрели", "")
 
-console.log(answers);
+
+let personalMovieDB= {
+    count: numberOfFilms,
+    movies:{},
+    actors:{},
+    genres: [],
+    privat: false
+};
+const a = prompt("Один из последних просмотренных фильмов", ""),
+      b = prompt ("На сколько оцените его?", `${meltText}`),
+      c = prompt("Один из последних просмотренных фильмов", ""),
+      d = prompt ("На сколько оцените его?", `${meltText}`);
+
+personalMovieDB.movies[a] = b;
+personalMovieDB.movies[c] = d;
+console.log(personalMovieDB);
+
 
 
